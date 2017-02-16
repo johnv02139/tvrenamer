c5b92e7c2b640527560127d491b945826275eb50
John Valente <johnv02139@yahoo.com>
2017-02-15 22:48:26 -0800

    Don't try to mock UserPreferences.
    The test creates a mock version of an instance of UserPreferences.  But, it doesn't do anything with it.  The FileEpisode class acquires the singleton instance of UserPreferences, within its own source code.  Creating a mock instance does nothing to affect this.
    
    If the static method, UserPreferences.getInstance(), could be mocked, that could work.  But, Mockito explicitly does not mock static methods.
    
    The best solution, then, is to use a genuine instance of UserPreferences.  And instead of providing a mock value to be returned upon "get", we actually set that value, and then the get returns it.
    
    One concern might be that the "set" would change the user's actual stored preferences, but "store" is only called when the UI dialog is dismissed.  Setting the value within the test does not affect the saved value.
    
    Also, since our expected values of moved files does not include a directory, be sure to set "moveEnabled" to false.

--- src/test/org/tvrenamer/model/FileEpisodeTest.java
+++ src/test/org/tvrenamer/model/FileEpisodeTest.java
@@ -7,7 +7,6 @@ import static org.mockito.Mockito.mock;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import org.tvrenamer.controller.ShowInformationListener;
 
@@ -29,7 +28,9 @@ public class FileEpisodeTest {
     @Before
     public void setUp() throws Exception {
         testFiles = new ArrayList<>();
-        prefs = Mockito.mock(UserPreferences.class);
+        prefs = UserPreferences.getInstance();
+        prefs.setRenameReplacementString("%S [%sx%e] %t");
+        prefs.setMoveEnabled(false);
         mockListener = mock(ShowInformationListener.class);
     }
 
@@ -53,8 +54,6 @@ public class FileEpisodeTest {
         season5.addEpisode(episodeNum, title, new Date());
         show.setSeason(seasonNum, season5);
         ShowStore.addShow(showName, show);
-
-        Mockito.when(prefs.getRenameReplacementString()).thenReturn("%S [%sx%e] %t");
 
         FileEpisode episode = new FileEpisode(showName, seasonNum, episodeNum, resolution, file);
         episode.setStatus(EpisodeStatus.DOWNLOADED);
