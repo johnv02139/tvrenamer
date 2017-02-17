b95f2d405b5bce7464e02ddaf7627f4adf97b78e
John Valente <johnv02139@yahoo.com>
2017-02-17 00:32:04 -0800

    Move method areSameDisk.

--- src/main/java/org/tvrenamer/controller/util/FileUtilities.java
+++ src/main/java/org/tvrenamer/controller/util/FileUtilities.java
@@ -90,4 +90,18 @@ public class FileUtilities {
         }
         return ok;
     }
+
+    public static boolean areSameDisk(String pathA, String pathB) {
+        File[] roots = File.listRoots();
+        if (roots.length < 2) {
+            return true;
+        }
+        for (File root : roots) {
+            String rootPath = root.getAbsolutePath();
+            if (pathA.startsWith(rootPath)) {
+                return pathB.startsWith(rootPath);
+            }
+        }
+        return false;
+    }
 }
