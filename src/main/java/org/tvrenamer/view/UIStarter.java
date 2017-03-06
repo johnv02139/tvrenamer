2f39e63cd44fc47cfa4cf2f48b173b03a28c94b6
John Valente <johnv02139@yahoo.com>
2017-03-05 20:28:32 -0800

    Set UI thread priority.

--- src/main/java/org/tvrenamer/view/UIStarter.java
+++ src/main/java/org/tvrenamer/view/UIStarter.java
@@ -587,6 +587,12 @@ public final class UIStarter implements Observer,  AddEpisodeListener {
             shell.open();
             resultsTable.setFocus();
 
+            // TODO: trying to get the UI thread to be more responsive.
+            // So far, this doesn't seem to have much, if any, effect.
+            // But it's probably part of the solution, even if it's not
+            // sufficient, so leave it in here.
+            Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+
             // Load the preload folder into the episode map, which will call
             // us back with the list of files once they've been loaded.
             episodeMap.subscribe(this);
