9adfc5ae7a412a4aeff498b1acf1d011d1293d36
John Valente <johnv02139@yahoo.com>
2017-03-08 23:48:41 -0800

    Add removeWhileEmpty and logger.

--- src/main/java/org/tvrenamer/controller/util/FileUtilities.java
+++ src/main/java/org/tvrenamer/controller/util/FileUtilities.java
@@ -1,8 +1,10 @@
 package org.tvrenamer.controller.util;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 public class FileUtilities {
+    private static Logger logger = Logger.getLogger(FileUtilities.class.getName());
 
     public static boolean areSameDisk(String pathA, String pathB) {
         File[] roots = File.listRoots();
@@ -16,5 +18,37 @@ public class FileUtilities {
             }
         }
         return false;
+    }
+
+    public static boolean removeWhileEmpty(File dir) {
+        if (dir == null) {
+            return false;
+        }
+        if (!dir.exists()) {
+            return false;
+        }
+        if (!dir.isDirectory()) {
+            return false;
+        }
+
+        String[] files = dir.list(null);
+        if (files == null) {
+            // This shouldn't happen.
+            return false;
+        }
+        if (files.length > 0) {
+            // If the directory is not empty, then doing nothing is correct,
+            // and we have succeeded.
+            return true;
+        }
+        File parent = dir.getParentFile();
+        boolean success = dir.delete();
+        if (success) {
+            logger.info("removed empty directory " + dir);
+            if (parent != null) {
+                return removeWhileEmpty(parent);
+            }
+        }
+        return success;
     }
 }
