cde8b369a8dbd7e853cca3b135d4029342ad6952
John Valente <johnv02139@yahoo.com>
2017-05-13 22:17:34 -0700

    Improvements to Launcher.java
    Use a regular static method for logging initialization instead of a static initializer block.
    
    Also break out the UI launcher into a separate static method.  I intend to change it so that Launcher could potentially launch different functionality depending on the command-line argument.  This is a first step to allowing it to do so.
    
    Prepare the main method to be able to dispatch to different functionality, depending on command-line arguments.
    
    Add a logger, and use it for an info message that we're launching the UI.

--- src/main/java/org/tvrenamer/controller/Launcher.java
+++ src/main/java/org/tvrenamer/controller/Launcher.java
@@ -8,8 +8,10 @@ import org.tvrenamer.view.UIStarter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 class Launcher {
+    private static final Logger logger = Logger.getLogger(Launcher.class.getName());
 
     private static void initializeLogger() {
         // Find logging.properties file inside jar
@@ -40,14 +42,22 @@ class Launcher {
      * All this application does is run the UI, with no arguments.  Configuration
      * comes from the PREFERENCES_FILE (see Constants.java).  But in the future,
      * it might be able to do different things depending on command-line arguments.
+     */
+    public static int launchUi() {
+        logger.info("starting UI...");
+        UIStarter ui = new UIStarter();
+        return ui.run();
+    }
+
+    /**
+     * Run a program; currently hard-coded to launchUi(), but can be expanded.
      *
      * @param args
      *    not actually processed, at this time
      */
     public static void main(String[] args) {
         initializeLogger();
-        UIStarter ui = new UIStarter();
-        int status = ui.run();
+        int status = launchUi();
         tvRenamerThreadShutdown();
         System.exit(status);
     }
