bb5d143b177c463f60254dcb382ba95be65b604b
John Valente <johnv02139@yahoo.com>
2017-05-17 23:12:39 -0700

    Add package for classes to help with development/testing

---
+++ src/main/java/org/tvrenamer/devel/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * The "devel" sub-package is for code which may not really be part of the
+ * TVRenamer program, and might not be accessible from the UI at all, but which
+ * is useful while doing development.
+ *
+ * Then again, it might be accessible.  Some of it could turn out to be useful
+ * to end users.
+ *
+ * As presently constructed, these files would be included in an application.
+ * But this branch is in the early stages of development.  We could eventually
+ * choose to move these files, or incorporate them, or configure launch4j to
+ * specifically exclude this package, etc., etc.  It doesn't need to all be
+ * figured out now.
+ *
+ */
+package org.tvrenamer.devel;
