b6a24a1bd25c1f2207b049da996a1431aad8fac9
John Valente <johnv02139@yahoo.com>
2018-06-09 11:22:55 -0700

    Rename preferences dialog cancel button to close

--- src/main/java/org/tvrenamer/model/util/Constants.java
+++ src/main/java/org/tvrenamer/model/util/Constants.java
@@ -88,7 +88,7 @@ public class Constants {
     }
 
     public static final String QUIT_LABEL = "Quit";
-    public static final String CANCEL_LABEL = "Cancel";
+    public static final String CLOSE_LABEL = "Close";
     public static final String SAVE_LABEL = "Save";
     public static final String ERROR_LABEL = "Error";
     public static final String EXIT_LABEL = "Exit";
