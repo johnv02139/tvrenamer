50206287747fbc165473c21d16952a5e0c6fa537
John Valente <johnv02139@yahoo.com>
2018-06-13 02:33:58 -0700

    Change createDestDirButton to createDestDirControls
    It used to return a button, but the variable is part of the instance, so we can set it in the called method.  Move the creation of destDirText into the same method, and rename it appropriately.

--- src/main/java/org/tvrenamer/view/PreferencesDialog.java
+++ src/main/java/org/tvrenamer/view/PreferencesDialog.java
@@ -213,10 +213,12 @@ class PreferencesDialog extends Dialog {
         return box;
     }
 
-    private Button createDestDirButton(Composite group) {
-        final Button button = new Button(group, SWT.PUSH);
-        button.setText(DEST_DIR_BUTTON_TEXT);
-        button.addListener(SWT.Selection, event -> {
+    private void createDestDirControls(Composite group) {
+        destDirText = createText(prefs.getDestinationDirectoryName(), group, false);
+
+        destDirButton = new Button(group, SWT.PUSH);
+        destDirButton.setText(DEST_DIR_BUTTON_TEXT);
+        destDirButton.addListener(SWT.Selection, event -> {
             DirectoryDialog directoryDialog = new DirectoryDialog(preferencesShell);
 
             directoryDialog.setFilterPath(prefs.getDestinationDirectoryName());
@@ -227,8 +229,6 @@ class PreferencesDialog extends Dialog {
                 destDirText.setText(dir);
             }
         });
-
-        return button;
     }
 
     /*
@@ -408,8 +408,7 @@ class PreferencesDialog extends Dialog {
                                                 true, generalGroup, GridData.END, 1);
 
         createLabel(DEST_DIR_TEXT, DEST_DIR_TOOLTIP, generalGroup);
-        destDirText = createText(prefs.getDestinationDirectoryName(), generalGroup, false);
-        destDirButton = createDestDirButton(generalGroup);
+        createDestDirControls(generalGroup);
 
         createSeasonPrefixControls(generalGroup);
 
