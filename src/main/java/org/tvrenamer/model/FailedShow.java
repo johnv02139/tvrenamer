88abf3ad06a0256aca68a0ebe54ca3ef49867d30
John Valente <johnv02139@yahoo.com>
2018-06-23 01:52:55 -0700

    FailedShow.java: Check in showfinder branch.

--- src/main/java/org/tvrenamer/model/FailedShow.java
+++ src/main/java/org/tvrenamer/model/FailedShow.java
@@ -2,14 +2,24 @@ package org.tvrenamer.model;
 
 public class FailedShow extends Show {
 
-    private final TVRenamerIOException ioe;
+    private final GenericException ioe;
 
-    public TVRenamerIOException getException() {
+    public GenericException getException() {
         return ioe;
     }
 
-    public FailedShow(String id, String name, TVRenamerIOException err) {
-        super(id, name);
-        ioe = err;
+    public FailedShow(String id, String name, String url, GenericException e) {
+        super(id, name, url);
+        ioe = e;
+    }
+
+    public FailedShow(String id, String name, TVRenamerIOException e) {
+        super(id, name, "");
+        ioe = new GenericException(e.getMessage());
+    }
+
+    public FailedShow(String message) {
+        super("", message, "");
+        ioe = null;
     }
 }
