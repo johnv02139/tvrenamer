51072a230d520b86c7490006e33a76c2f61ffbb3
John Valente <johnv02139@yahoo.com>
2018-06-23 01:52:56 -0700

    StdOutConsoleFormatter.java: Check in showfinder branch.

--- src/main/java/org/tvrenamer/model/util/StdOutConsoleFormatter.java
+++ src/main/java/org/tvrenamer/model/util/StdOutConsoleFormatter.java
@@ -2,42 +2,48 @@ package org.tvrenamer.model.util;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 public class StdOutConsoleFormatter extends Formatter {
 
-    static final String FORMAT_STRING = "[yyyy/MM/dd kk:mm:ss,SSS] ";
-
     @Override
     public String format(LogRecord rec) {
         StringBuilder buffer = new StringBuilder(1000);
 
-        // Date
-        ZoneId zone = ZoneId.systemDefault();
-        DateTimeFormatter sdf = DateTimeFormatter.ofPattern(FORMAT_STRING).withZone(zone);
-        Instant recordInstant = Instant.ofEpochMilli(rec.getMillis());
-        buffer.append(sdf.format(recordInstant));
-
         // Level
-        if (rec.getLevel() == Level.WARNING) {
+        if (rec.getLevel() == Level.SEVERE) {
+            buffer.append("SVRE ");
+        } else if (rec.getLevel() == Level.WARNING) {
             buffer.append("WARN ");
+        } else if (rec.getLevel() == Level.CONFIG) {
+            buffer.append("CNFG ");
+        } else if (rec.getLevel() == Level.FINER) {
+            buffer.append("FINR ");
+        } else if (rec.getLevel() == Level.FINEST) {
+            buffer.append("FNST ");
         } else {
             buffer.append(rec.getLevel()).append(" ");
         }
 
+        // Date
+        String formatString = "[kk:mm:ss,SSS] ";
+        SimpleDateFormat sdf = new SimpleDateFormat(formatString);
+        Date date = new Date(rec.getMillis());
+        buffer.append(sdf.format(date));
+
         // Class name (not package), method name
-        buffer.append(rec.getSourceClassName().substring(rec.getSourceClassName().lastIndexOf(".") + 1)).append("#");
-        buffer.append(rec.getSourceMethodName()).append(" ");
+        // buffer.append(rec.getSourceClassName().substring(rec.getSourceClassName().lastIndexOf(".") + 1)).append("#");
+        // buffer.append(rec.getSourceMethodName()).append(" ");
 
         // Message
         buffer.append(rec.getMessage());
 
         // Stacktrace
+        //noinspection ThrowableResultOfMethodCallIgnored
         Throwable throwable = rec.getThrown();
         if (throwable != null) {
             StringWriter sink = new StringWriter();
@@ -46,7 +52,6 @@ public class StdOutConsoleFormatter extends Formatter {
         }
 
         // Note: No need to add a newline as that is added by the Handler
-
         return buffer.toString();
     }
 }
